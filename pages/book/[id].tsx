8e93b438ae26133da16ae1f461a7295a1d06817d
galangramadan <galangynr@gmail.com>
2023-09-23 01:49:05 +0700

    fix: book details routing issue and change it to ssg

--- pages/book/[id].tsx
+++ pages/book/[id].tsx
@@ -1,53 +1,53 @@
 import Breadcrumbs from "@/components/molecules/Breadcrumbs";
 import Navbar from "@/components/organisms/Navbar";
 import Footer from "@/components/organisms/Footer";
-import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Link from "next/link";
-import { getSingleBook } from "@/services/book.services";
+import { getAllBooks, getSingleBook } from "@/services/book.services";
+import { GetStaticPropsContext } from "next";
 
-const BookDetails = () => {
-  const [bookDetails, setBookDetails] = useState({} as bookDetails);
+export const getStaticPaths = async () => {
+  const res = await getAllBooks();
+  const books = res.data;
+  const paths = books.map((book: bookDetails) => ({
+    params: { id: book.id.toString() },
+  }));
 
-  const router = useRouter();
-  const id = router.query.id;
+  return { paths, fallback: false };
+};
 
-  const getData = async (param: number) => {
-    const data = await getSingleBook(param);
-    if (data.data) {
-      setBookDetails(data.data);
-    }
+export const getStaticProps = async (context: GetStaticPropsContext) => {
+  const { params } = context;
+  const id = params?.id;
+  const res = await getSingleBook(parseInt(id as string));
+
+  return {
+    props: res.data,
   };
-  useEffect(() => {
-    if (id !== undefined) {
-      const param = parseInt(id as string);
-      getData(param);
-    }
-  }, [id]);
+};
 
-  console.log(bookDetails);
-
+const BookDetails = (props: bookDetails) => {
   return (
     <>
       <Navbar />
       <Breadcrumbs>
-        Buku / {Object.keys(bookDetails).length > 0 ? bookDetails.title : ""}
+        Buku / {Object.keys(props).length > 0 ? props.title : ""}
       </Breadcrumbs>
       <div className="container px-4 md:px-8 py-8">
-        {Object.keys(bookDetails).length > 0 ? (
+        {Object.keys(props).length > 0 ? (
           <>
             <div className="flex items-start">
               <img
-                src={`../${bookDetails.image}`}
+                src={`../${props.image}`}
                 alt="alternative"
                 className="w-1/4 object-contain"
               />
               <div className="pl-12 flex flex-col gap-4">
-                <h3>{bookDetails.title}</h3>
+                <h3>{props.title}</h3>
                 <img className="w-12 -ml-2" src="/svg/star5.svg" alt="Rating" />
-                <p>Penulis : {bookDetails.author}</p>
+                <p>Penulis : {props.author}</p>
                 <h5>Deskripsi Buku</h5>
-                <p>{bookDetails.description}</p>
+                <p>{props.description}</p>
               </div>
             </div>
             <div className="text-center mt-24">
