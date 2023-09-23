0e552127a4db56ffdbc4888401181e7d4094502c
galangramadan <galangynr@gmail.com>
2023-09-23 20:10:49 +0700

    refactor: use card book components in books by category page

--- pages/categories/[category].tsx
+++ pages/categories/[category].tsx
@@ -2,9 +2,9 @@ import Breadcrumbs from "@/components/molecules/Breadcrumbs";
 import Navbar from "@/components/organisms/Navbar";
 import Footer from "@/components/organisms/Footer";
 import React from "react";
-import Link from "next/link";
 import { getAllCategories, getBooksByCategory } from "@/services/book.services";
 import { GetStaticPropsContext } from "next";
+import CardBookCategory from "@/components/molecules/CardBookCategory";
 
 type Props = {
   result: bookDetails[];
@@ -41,33 +41,16 @@ const categoryBooks = (props: Props) => {
       <div className="container px-4 sm:px-8">
         <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
           {Object.keys(result).length > 0 ? (
-            result.map((item) => (
-              <div
-                className="relative overflow-hidden bg-white shadow-md rounded-xl"
-                key={item.id}
-              >
-                <Link href={`/book/${item.id}`}>
-                  <img
-                    className="m-auto mt-8 mb-4 overflow-hidden w-36 h-44"
-                    src={item.image}
-                    alt="picturebook"
-                  />
-                </Link>
-                <div className="px-4 text-left">
-                  <h5 className="overflow-hidden overflow-ellipsis whitespace-nowrap">
-                    {item.title}
-                  </h5>
-                  <p className="">{item.author}</p>
-                  <p className="overflow-hidden overflow-ellipsis whitespace-nowrap p-small">
-                    {item.description}
-                  </p>
-                  <span className="rating-more">
-                    <img className="rating" src="/svg/star5.svg" alt="Rating" />
-                    <Link className="more" href={`/book/${item.id}`}>
-                      Selengkapnya
-                    </Link>
-                  </span>
-                </div>
+            result.map((item, i) => (
+              <div key={i}>
+                <CardBookCategory
+                  id={item.id}
+                  title={item.title}
+                  author={item.author}
+                  description={item.description}
+                  image={item.image}
+                  category_id={item.category_id}
+                />
               </div>
             ))
           ) : (
